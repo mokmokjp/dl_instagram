2808213e17799ad7a54872fcb31e2e5301770042
Masaki Nakahigashi <chuto775@gmail.com>
2017-12-28 03:30:53 +0900

    一括DL機能追加

--- src/src.js
+++ src/src.js
@@ -1,25 +1,44 @@
 (function() {
-  // プロフィール画像を取得
-  var temp = document.getElementsByClassName('_9bt3u');
-  if (temp.length > 0) {
-    // 一覧ページの場合
-    var elem = document.getElementsByClassName('_sxolz');
-    var img = elem[0].getElementsByTagName('img');
-  } else {
-    // 詳細ページの場合
-    var elem = document.getElementsByClassName('_4rbun');
-    var img = elem[0].getElementsByTagName('img');
+  // ユーザー名を取得
+  if (document.getElementsByTagName('h1')[0] !== undefined) {
+    user_name = document.getElementsByTagName('h1')[0].title;
+  } else if (document.getElementsByClassName('_iadoq')[0] !== undefined) {
+    user_name = document.getElementsByClassName('_iadoq')[0].title;
   }
-  // ダウンロードしたいコンテンツ、MIMEType、ファイル名
-  var imgpath  = img[0].src;
-  var name     = img[0].src;
-  var mimeType = 'image/jpeg';
-  // BOMは文字化け対策
-  var bom  = new Uint8Array([0xEF, 0xBB, 0xBF]);
-  var blob = new Blob([bom, imgpath], {type : mimeType});
-  var a = document.createElement('a');
-  a.download = name;
-  a.target   = '_blank';
-  a.href = name;
-  a.click();
+
+  // 画像要素を取得
+  if (document.getElementsByClassName('_sxolz').length > 0) {
+    // 一覧ページでモーダルウィンドウ表示時の場合
+    // 詳細ページの場合
+    var wrap_elements = document.getElementsByClassName('_sxolz');
+    var img_elements = wrap_elements[0].getElementsByTagName('img');
+    var process = true;
+  } else {
+    // 一覧ページでモーダルウィンドウ非表示時の場合
+    var img_elements = document.getElementsByClassName('_2di5p');
+    if (confirm('ページ内に表示中の画像をすべてダウンロードします')) {
+      var process = true;
+    } else {
+      var process = false;
+    }
+  }
+
+  // 画像ダウンロード処理
+  if (process) {
+    for (var i = 0; i < img_elements.length; i++) {
+        // var mimeType = 'image/jpeg'; // MIMEType
+        // var bom  = new Uint8Array([0xEF, 0xBB, 0xBF]); // BOMは文字化け対策
+        // var blob = new Blob([bom, img_path], {type : mimeType});
+      var img_path = img_elements[i].src; // ダウンロードしたい画像のパス
+      var a = document.createElement('a');
+      a.target   = '_blank';
+      a.download = user_name + img_path;
+      a.href = img_path;
+      a.click();
+    }
+  }
+
+// TODO: 一括DLの時画像がトリミング状態になっている
+// ダウンロードファイル名
+
 })();
