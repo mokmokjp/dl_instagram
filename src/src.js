b8cebaeb4a725be63bfd3ad6b66b4bde92081a6e
Masaki Nakahigashi <chuto775@gmail.com>
2018-06-18 02:26:24 +0900

    バージョンアップ

--- src/src.js
+++ src/src.js
@@ -2,15 +2,15 @@
   // ユーザー名を取得
   if (document.getElementsByTagName('h1')[0] !== undefined) {
     user_name = document.getElementsByTagName('h1')[0].title;
-  } else if (document.getElementsByClassName('_iadoq')[0] !== undefined) {
-    user_name = document.getElementsByClassName('_iadoq')[0].title;
+  } else if (document.getElementsByClassName('e1e1d')[0] !== undefined) {
+    user_name = document.getElementsByClassName('e1e1d')[0].title;
   }
 
   // 画像要素を取得
-  if (document.getElementsByClassName('_sxolz').length > 0) {
+  if (document.getElementsByClassName('_97aPb ').length > 0) {
     // 一覧ページでモーダルウィンドウ表示時の場合
     // 詳細ページの場合
-    var wrap_elements = document.getElementsByClassName('_sxolz');
+    var wrap_elements = document.getElementsByClassName('_97aPb ');
     var img_elements = wrap_elements[0].getElementsByTagName('img');
     var process = true;
   } else {
@@ -26,15 +26,18 @@
   // 画像ダウンロード処理
   if (process) {
     for (var i = 0; i < img_elements.length; i++) {
-        // var mimeType = 'image/jpeg'; // MIMEType
-        // var bom  = new Uint8Array([0xEF, 0xBB, 0xBF]); // BOMは文字化け対策
-        // var blob = new Blob([bom, img_path], {type : mimeType});
+      var bom  = new Uint8Array([0xEF, 0xBB, 0xBF]);
+      var mimeType = 'image/jpeg'; // MIMEType
       var img_path = img_elements[i].src; // ダウンロードしたい画像のパス
-      var a = document.createElement('a');
-      a.target   = '_blank';
-      a.download = user_name + img_path;
-      a.href = img_path;
-      a.click();
+      var blob = new Blob([bom, img_path], {"type" : mimeType});
+      var dl_link = document.createElement('a');
+      wrap_elements[0].appendChild(dl_link);
+      dl_link.target    = '_blank';
+      dl_link.innerText = 'ダウンロードリンク';
+      dl_link.download  = user_name + img_path;
+      dl_link.href      = img_path;
+      dl_link.click();
+      // wrap_elements[0].removeChild(dl_link);
     }
   }
 
