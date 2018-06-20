fc294f5512e49049c83778db81dea6d1bcbba4c2
Masaki Nakahigashi <chuto775@gmail.com>
2018-06-20 20:57:31 +0900

    新しいタブで開くだけでダウンロードしない不具合を解消。ダウンロードファイル名にユーザー名を含めるようにした。

--- src/src.js
+++ src/src.js
@@ -1,9 +1,10 @@
 (function() {
   // ユーザー名を取得
   if (document.getElementsByTagName('h1')[0] !== undefined) {
-    user_name = document.getElementsByTagName('h1')[0].title;
+    var user_name = document.getElementsByTagName('h1')[0].title;
   } else if (document.getElementsByClassName('e1e1d')[0] !== undefined) {
-    user_name = document.getElementsByClassName('e1e1d')[0].title;
+    var user_name_a = document.getElementsByClassName('e1e1d')[0].getElementsByTagName('a');
+    var user_name = user_name_a[0].innerText
   }
 
   // 画像要素を取得
@@ -26,22 +27,38 @@
   // 画像ダウンロード処理
   if (process) {
     for (var i = 0; i < img_elements.length; i++) {
-      var bom  = new Uint8Array([0xEF, 0xBB, 0xBF]);
-      var mimeType = 'image/jpeg'; // MIMEType
       var img_path = img_elements[i].src; // ダウンロードしたい画像のパス
-      var blob = new Blob([bom, img_path], {"type" : mimeType});
-      var dl_link = document.createElement('a');
-      wrap_elements[0].appendChild(dl_link);
-      dl_link.target    = '_blank';
-      dl_link.innerText = 'ダウンロードリンク';
-      dl_link.download  = user_name + img_path;
-      dl_link.href      = img_path;
-      dl_link.click();
-      // wrap_elements[0].removeChild(dl_link);
+      // Ajaxで取得
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', img_path);
+      xhr.responseType = 'blob';
+      xhr.addEventListener('load', function(event){
+        // console.log(event.target.response);
+        var res_img = event.target.response;
+        // blobオブジェクトを作る
+        var bom  = new Uint8Array([0xEF, 0xBB, 0xBF]);
+        var mimeType = 'image/jpeg'; // MIMEType
+        var blob = new Blob([bom, res_img], {"type" : mimeType});
+        // a要素を作る
+        var dl_link = document.createElement('a');
+        var url = window.URL || window.webkitURL;
+        var blobUrl = url.createObjectURL(blob);
+        wrap_elements[0].appendChild(dl_link);
+        dl_link.target    = '_blank';
+        dl_link.innerText = 'ダウンロードリンク';
+        var arrFilename = img_path.split( /\// );
+        var filename_index = arrFilename.length - 1;
+        dl_link.download  = user_name + '_' + arrFilename[filename_index];
+        dl_link.href      = blobUrl;
+        // ダウンロード処理
+        dl_link.click();
+        // a要素を取り除く
+        wrap_elements[0].removeChild(dl_link);
+      });
+      xhr.send();
     }
   }
+})();
 
 // TODO: 一括DLの時画像がトリミング状態になっている
-// ダウンロードファイル名
 
-})();
